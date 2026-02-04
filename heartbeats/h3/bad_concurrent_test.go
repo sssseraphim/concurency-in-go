69b590b192d26a98c95a875f22bddba8f72d87b3
sssseraphim <tarasovv2004@ya.ru>
2026-02-04 19:08:22 +0300

    heartbeat with pulse tests

--- heartbeats/h3/bad_concurrent_test.go
+++ heartbeats/h3/bad_concurrent_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+	"time"
 )
 
 func TestDoWork_GeneratesAllNumbers(t *testing.T) {
@@ -19,5 +20,35 @@ func TestDoWork_GeneratesAllNumbers(t *testing.T) {
 			t.Errorf("index %v, expected %v, received %v", i, intSlice[i], r)
 		}
 		i++
+	}
+}
+
+func TestDoWorkWithPulse_GeneratesAllNumbers(t *testing.T) {
+	done := make(chan any)
+	defer close(done)
+
+	intSlice := []int{1, 2, 3, 4, 5}
+	heartbeat, res := DoWorkWithPulse(done, time.Second, intSlice...)
+	<-heartbeat
+
+	i := 0
+	for {
+		select {
+		case r, ok := <-res:
+			if !ok {
+				return
+			} else if r != intSlice[i] {
+				t.Errorf("index %v: expected %v, but received %v,",
+					i,
+					intSlice[i],
+					r,
+				)
+			}
+			i++
+		case <-heartbeat:
+		case <-time.After(time.Second * 2):
+			t.Fatal("test timed out")
+
+		}
 	}
 }
--- heartbeats/h3/heartbeat.go
+++ heartbeats/h3/heartbeat.go
@@ -29,3 +29,37 @@ func DoWork(
 	}()
 	return heartbeat, res
 }
+
+func DoWorkWithPulse(
+	done <-chan any,
+	pulseInterval time.Duration,
+	nums ...int,
+) (<-chan any, <-chan int) {
+	heartbeat := make(chan any, 1)
+	res := make(chan int)
+	go func() {
+		defer close(heartbeat)
+		defer close(res)
+
+		time.Sleep(2 * time.Second)
+
+		pulse := time.Tick(pulseInterval)
+	numLoop:
+		for _, n := range nums {
+			for {
+				select {
+				case <-done:
+					return
+				case <-pulse:
+					select {
+					case heartbeat <- struct{}{}:
+					default:
+					}
+				case res <- n:
+					continue numLoop
+				}
+			}
+		}
+	}()
+	return heartbeat, res
+}
